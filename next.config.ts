0424562a7693fba9e9bb1fb9662e8e1f649882ea
Harisa Ginting <harisaginting@gmail.com>
2026-01-12 14:51:18 +0700

    ci: fix wagmi config

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     remotePatterns: [{ hostname: "assets.basehub.com" }, { hostname: "basehub.earth" }],
   },
+  // Empty turbopack config to silence the warning about webpack config
+  turbopack: {},
 } satisfies NextConfig;
 
 export default nextConfig;
